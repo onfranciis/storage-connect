ed9e4776f83577f1f00456d9292a1460036b0d0d
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-08-15 19:11:15 +0100

    Added Theme, Temp And Others

---
+++ Temp.js
@@ -0,0 +1,67 @@
+{
+  /* <div
+              className={styles.cards}
+              style={{
+                border: view == "List" ? "1px solid #D9D9D9" : "",
+                padding: view == "List" ? 40 : "",
+                borderRadius: view == "List" ? 20 : "",
+              }}
+            >
+              <MinimalCard
+                Orientation={view}
+                Pic="/unsplash_OpV94f2edwE.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+
+              <MinimalCard
+                Orientation={view}
+                Pic="/image2.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+
+              <MinimalCard
+                Orientation={view}
+                Pic="/unsplash_-BQBmDlDE70.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+              <MinimalCard
+                Orientation={view}
+                Pic="/unsplash_-BQBmDlDE70.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+
+              <MinimalCard
+                Orientation={view}
+                Pic="/unsplash_OpV94f2edwE.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+
+              <MinimalCard
+                Orientation={view}
+                Pic="/image2.png"
+                Location="Jersey City, NJ"
+                Units="89"
+                Occupancy="50"
+                Size="40,233"
+              />
+            </div>
+
+            <div className={styles.viewMore}>
+              <button>View More Listings</button>
+            </div> */
+}
